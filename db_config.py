c8653383400c856307de39e170490ec19c30e01a
Xiaosong <156469597+Xiaosong50@users.noreply.github.com>
2025-04-28 16:09:31 -0500

    Delete db_config.py

--- db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='admin',
-        password='Lxs,123321',
-        database='student_survey'
-    )
